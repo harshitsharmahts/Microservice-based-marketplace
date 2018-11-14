e8d68bf2dc9e60fe84d4944fffad99c43f611a82
Harshit Sharma <harshitsharmahts@gmail.com>
2018-11-14 23:50:14 +0530

    Initial Upload on Github

---
+++ src/main/java/com/ecommerce/config/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.ecommerce.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaServer
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
