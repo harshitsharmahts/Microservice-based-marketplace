e019b3bb33cc4f734d7f5896e0d5917f511d564c
Harshit Sharma <harshitsharmahts@gmail.com>
2018-11-15 00:12:59 +0530

    Spring Boot App

---
+++ src/main/java/com/ecommerce/DbItem/DbItemApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.DbItem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbItemApplication.class, args);
+	}
+}
