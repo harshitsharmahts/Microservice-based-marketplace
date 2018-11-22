6f8b400ab9fb29510b7c99b56ed69fe0c36eca74
Harshit Sharma <harshitsharmahts@gmail.com>
2018-11-22 21:22:20 +0530

    Initial Spring Boot

---
+++ src/main/java/com/ecommerce/LoginSignupService/LoginSignupServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.LoginSignupService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSignupServiceApplication.class, args);
+	}
+}
